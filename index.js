e86dd53996f9d06fbea33bfda847b2f773609a3c
p0syd0n <ajeff9752@gmail.com>
2023-06-02 20:38:33 +0000

    tried to fix the encryption

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ async function get_data() {
 }
 
 async function add_user(username, password, public_key, private_key) {
+  let hashed = await hash(password)
   return new Promise((resolve, reject) => {
     var options = {
       method: 'POST',
@@ -93,7 +94,7 @@ async function add_user(username, password, public_key, private_key) {
         'x-apikey': '61638a95f2bbe9a65b4b337baeff07152897e',
         'content-type': 'application/json',
       },
-      body: { username, password, public_key, private_key },
+      body: { username, password: hashed, public_key, private_key },
       json: true,
     };
 
@@ -121,7 +122,7 @@ async function checkLogin(username, password) {
         return {
           'username': username,
           'public_key': user_dict['public_key'],
-          'private_key': decrypt(user_dict['private_key'])
+          'private_key': user_dict['private_key']
         };
       }
     }
@@ -163,11 +164,10 @@ app.get('/main', (req, res) => {
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   let verify = await checkLogin(username, password);
-  console.log(verify)
   if (verify != null) {
     req.session.username = verify['username'];
-    req.session.public_key = verify['public_key'];
-    req.session.private_key = verify['private_key'];
+    req.session.publicKey = verify['public_key'];
+    req.session.privateKey = verify['private_key'];
     res.redirect('/main');
   } else {
     res.sendStatus(401);
@@ -184,8 +184,20 @@ app.post('/add_account', async (req, res) => {
 });
 
 app.get('/encrypt', (req, res) => {
-  const public_key = req.query.public_key
-  const private_key = req.body.private_keys
+  const data = req.query.data
+  const publicKey = req.query.publicKey
+  if (publicKey === `-----BEGIN PUBLIC KEY-----
+  MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtkwwSc0B88G1RqNUAHWY
+  /RkRUZO2Yz6Y4xJR1LFcH+xulnRepEUpfvwXePzrY2ymBOFeMGN89uRcgTgbZSrj
+  siSYJ1s7SYL+N33RLDXyL1dBv1an2M5h9vv6uLX7EQz7RcwR9abuOvRNLeiGUbL2
+  Ano+zhqLN3Gg0bNR0YkND7Lbj2RFPPI5Fs0/WypoJbAKtjqjNY0bkkMY7dy3I3uq
+  YEWn66RPdkpCOpgjkWB8hjp2UeUBKBSg6foCivtrRmX/17eNzp5xU5+YRAqmk+7Q
+  ZuNvm82mOobuNCCV+e5Cd54W3kbrZTRArGxbtXwG4HzXl+js5W9Im4GfgRNnhk7H
+  AQIDAQAB
+  -----END PUBLIC KEY-----`) {
+    console.log('SAME KEY')
+  }
+  return encryptRSA(data, publicKey)
 });
 
 app.get('/create_account', (req, res) => {
@@ -199,11 +211,55 @@ app.listen(3000, () => {
 
 // let data = await get_data();
 // console.log(data);
-let my_keys = await generate_keys();
-// let encrypted = encryptRSA('penis', my_keys.publicKey)
+// let my_keys = await generate_keys();
+// console.log(typeof my_keys.publicKey)
+// console.log(my_keys.publicKey)
+// console.log(my_keys.privateKey)
+// let encrypted = encryptRSA('beans', my_keys.publicKey)
 // console.log(encrypted);
-// console.log(decryptRSA(encrypted, my_keys.privateKey))
+// // console.log(decryptRSA(encrypted, my_keys.privateKey))
+// let response = await add_user('posydon', 'admin', my_keys.publicKey, my_keys.privateKey);
 
-// let response = await add_user('posydon', hash('admin'), my_keys.publicKey, encrypt(my_keys.privateKey));
 
-//console.log(await checkLogin('posydon', 'admin'));
+/*public:
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtkwwSc0B88G1RqNUAHWY
+/RkRUZO2Yz6Y4xJR1LFcH+xulnRepEUpfvwXePzrY2ymBOFeMGN89uRcgTgbZSrj
+siSYJ1s7SYL+N33RLDXyL1dBv1an2M5h9vv6uLX7EQz7RcwR9abuOvRNLeiGUbL2
+Ano+zhqLN3Gg0bNR0YkND7Lbj2RFPPI5Fs0/WypoJbAKtjqjNY0bkkMY7dy3I3uq
+YEWn66RPdkpCOpgjkWB8hjp2UeUBKBSg6foCivtrRmX/17eNzp5xU5+YRAqmk+7Q
+ZuNvm82mOobuNCCV+e5Cd54W3kbrZTRArGxbtXwG4HzXl+js5W9Im4GfgRNnhk7H
+AQIDAQAB
+-----END PUBLIC KEY-----
+*/
+
+/*private:
+-----BEGIN PRIVATE KEY-----
+MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC2TDBJzQHzwbVG
+o1QAdZj9GRFRk7ZjPpjjElHUsVwf7G6WdF6kRSl+/Bd4/OtjbKYE4V4wY3z25FyB
+OBtlKuOyJJgnWztJgv43fdEsNfIvV0G/VqfYzmH2+/q4tfsRDPtFzBH1pu469E0t
+6IZRsvYCej7OGos3caDRs1HRiQ0PstuPZEU88jkWzT9bKmglsAq2OqM1jRuSQxjt
+3Lcje6pgRafrpE92SkI6mCORYHyGOnZR5QEoFKDp+gKK+2tGZf/Xt43OnnFTn5hE
+CqaT7tBm42+bzaY6hu40IJX57kJ3nhbeRutlNECsbFu1fAbgfNeX6Ozlb0ibgZ+B
+E2eGTscBAgMBAAECggEADRSwOdl3sISns/EEFfDaSJ4H/bssURiEqrWNsOYFouZJ
+pKrmwCGU2c9vxT8bymN5UbwUsMLwzHwELcMHhzXBxy6ESv2rRkn7oSJ6dBJOZap8
+laTbkC11/t2jpuWrBXURojdjmgWPjmQplhfbbIkbiT+5mjsyHmk9RZLZFgCgMu4u
++EeDcUzcnhhwtlZQoOZKMCwu6zB4na+0ZRQ3M03RzwDUO4KqnCywH1R58Mof65yv
+iBVLGdu2uGsOgT0v3xEcE8xiQwx+OLFhKQCXq7jyH3sTnWLJkE/wkvMuJl/YIcLf
+/kSR9aUEMmkIlJYeEhVtNycwIH2NufcPwklvpPrB2QKBgQDgE1dr4aYy3BCs6SYY
+gtK9tAiDdychvG0/VPOsmoBt4Y73pv6V3gzWOjHiAfxgONsD6nnOq8i+9DWHBCev
+f27L91fGSr+7ViFQOmLzqPnCnkWAa7zoJzq14qOrguuhV2Upwhu71lChZ3QxLjWG
+FEy3RB63VKIXVRb7VYxo365KKQKBgQDQRRbmsL2iKWUim8ThIu0/DB/Cii2dxck3
+TZp3L1CMMdAa2+vsMIbXlwCx1kPu6mECzkLUFRQ8e5LoWFjnCNfTOHaV4OBgf9NA
+xRj0MR1AOWMoJusbvlx0ljqZOi7rwRJdGdoFOlzC7FwKE7h8sLJ2BYAJSNM5J2YM
+rZ9taLBhGQKBgQCcXGLQoTF8pBvuzPMMf53XSt31/xuLaxVA8GLpuxaLlq5RGnLk
+2YJTq2BLud8GtiX+4YwGL9FxjrAbe0bGLEKAiiTx9g89BqlQdq2+IdfHevI83vF/
+foB1k6VqdX0/pOCu9wirVq4FM0/dUcV/voKaECru+1vwtCvJTrUgKZZRoQKBgGm4
+6KsqRNkwkjpDQGnYxP4gNW8XnZhdcos0RZkC16yTDMTVpqk66Pg/mwarcciyCZTX
+B22JVcBuKmDvmBGPl5+NJgVnJf9WY/KZGtW6qNKVTxRuzSJEO8KjEBQASRk75QqF
+uPq2aag5O2+SGzCQscbfe+M6qJ52tYLyD+AGKKLhAoGAWSIcSwUiyo5JRoqB5Jk8
+k5mXtccr5lGlEk6PKCtHS+i7lfhJ690440hwyhhVHOQ0vOyZx8KzUXPKDfg7XRcY
+572o/ttp1C0wxsP8E/AlIlcWvjK6Qf2AleTM5CB3ZLKkDY39O4W4RfusEtvjXO9r
+IHr8eHs46Phgyx8cQzjhHls=
+-----END PRIVATE KEY-----
+*/
